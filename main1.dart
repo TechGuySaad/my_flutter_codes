b82cf4ba0a0a08d3e71f41a313790cdbb6ce8994
Saad Rahman <saadreh5@gmail.com>
2022-03-10 23:48:30 +0500

    hello world code

---
+++ main1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp( 
+    Center(
+          child:Text(
+              "hello flutter" ,
+              textDirection: TextDirection.ltr
+              )//Text
+          )//Center
+
+          ); //runApp
+}//main
